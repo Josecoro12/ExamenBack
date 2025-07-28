c4a7071dc1429ec61e0b0589c2cae3312a78d8cc
Jose Coronel <jose.coronel@est.ucacue.edu.ec>
2025-07-28 16:43:29 -0500

    hola

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth');
 const estudianteRoutes = require('./routes/estudiante');
 
+
+const cors = require('cors');
 const app = express();
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/auth', authRoutes);
